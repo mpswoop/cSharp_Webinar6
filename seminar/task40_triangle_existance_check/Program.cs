c5cb68aff9b61463bf23aa4aa7931afa754e808e
MP <mp@swoop.pro>
2023-07-25 21:32:25 +0300

    task40 - tiangle check

---
+++ seminar/task40_triangle_existance_check/Program.cs
@@ -0,0 +1,15 @@
+﻿// check if triangle with length input exists
+
+Console.WriteLine("Please Input length of Side A");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please Input length of Side B");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please Input length of Side C");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+if (sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC > sideB) 
+{
+    Console.WriteLine("triangle exists");
+}
+
+else Console.WriteLine("triangle doesn't exist");
